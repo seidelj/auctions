5c77a681fddb2c9308cd2ae69b3682e639f02e9b
joseph <seidel.jp@gmail.com>
2015-06-05 16:01:14 -0500

    Initialziation

---
+++ search.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+
+def main():
+
---
+++ sites.py
@@ -0,0 +1,279 @@
+
+sites = []
+
+sites.append({
+	"name": "Albersheims",
+	"url": "http://albersheims.com/",
+	"element": "id",
+	"searchbox": "query",
+})
+
+sites.append({
+	"name": "Baggers Auctions",
+	"url": "http://www.baggersauctions.com/",
+	"element": "id",
+	"searchbox": "search",
+})
+
+sites.append({
+	"name": "BST Auctions",
+	"url": "http://bst-auctions.com/",
+	"element": "id",
+	"searchbox": "searchTextBox",
+})
+
+sites.append({
+	"name": "Becket Auction Services",
+	"url": "http://auctions.beckett.com/",
+	"element": "id",
+	"searchbox": "searchTextBox",
+
+sites.append({
+	"name": "Clean Sweep Auctions",
+	"url": "http://www.cleansweepauctions.com/",
+	"element": "name",
+	"searchbox": "searchtext",
+})
+
+sites.append({
+	"name": "Collect Auctions",
+	"url": "http://www.collectauctions.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+})
+
+# This url will need to be refreshed.  It is powered by 3rd
+# party which means a new url will be generated for new auctions
+# Current auction begins June 13, 2015
+sites.append({
+	"name": "Fusco Auctions",
+	"url": "https://fusco.infinitebidding.com/?method=getCatalogLots&catalogref=PYF6W5MMW8",
+	"element": "id",
+	"searchbox": "af_searchField",
+})
+
+sites.append({
+	"name": "Giovanni Sports Auctions",
+	"url": "http://www.giovannisportsauctions.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+})
+
+sites.append({
+	"name": "Goldin Auctions",
+	"url": "http://goldinauctions.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+})
+
+sites.append({
+	"name": "Goodwin and Company",
+	"url": "http://www.goodwinandco.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+})
+
+sites.append({
+	"name": "Grey Flannel Auctions",
+	"url": "http://catalog.greyflannelauctions.com/catalog.aspx?category=9",
+	"element": "id",
+	"searchbox": "searchTextBox",
+})
+
+sites.append({
+	"name": "Heritage Auctions",
+	"url": "http://www.ha.com/",
+	"element": "id",
+	"searchbox": "search",
+})
+
+sites.append({
+	"name": "Huggins and Scott",
+	"url": "http://www.hugginsandscott.com",
+	"element": "name",
+	"searchbox": "search",
+})
+
+sites.append({
+	"name": "Iconic Auctions",
+	"url": "http://iconicauctions.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+})
+
+sites.append({
+	"name": "Inside the Park",
+	"url": "http://insidetheparkcollectibles.com/catalog.aspx",
+	"element": "id",
+	"searchbox":  "searchTextBox",
+})
+
+sites.append({
+	"name": "Kevin Savage Cards",
+	"url": "http://www.kevinsavagecards.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+})
+
+sites.append({
+	"name": "Lelands",
+	"url": "http://www.lelands.com/Home/SearchResult?Type=t&Description="
+	"element": None,
+	"searchbox": None,
+	"altMethod": "getQuery",
+})
+
+sites.append({
+	"name": "Love the game auctions",
+	"url": "https://loveofthegameauctions.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+	"results": "galleryList",
+})
+
+sites.append({
+	"name": "Mears online auctions",
+	"url": "https://loveofthegameauctions.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+	"results": "galleryList",
+})
+
+sites.append({
+	"name": "Memory Lane Inc."
+	"url": "http://www.memorylaneinc.com/site/gallery.aspx",
+	"element": "id",
+	"searchbox": "ctl00_pnlSidebar_i1_i0_txtSearch",
+	"altMethod": "searchBy",
+	"altId": "ctl00_pnlSidebar_i1_i0_lstSearch_Input",
+	"altKeys": "t",
+	"results": "ctl00_ContentPlaceHolder1_RadAjaxPanel1",
+})
+
+sites.append({
+	"name": "Mile High Card Club",
+	"url": "http://www.milehighcardco.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+	"results": "galleryList",
+})
+
+sites.append({
+	"name": "Paragon Auctions",
+	"url": "http://www.paragonauctionsite.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+	"altMethod": "searchBy",
+	"altId": "searchByDropDown",
+	"altKeys": "t",
+	"results": "imageTable",
+})
+
+sites.append({
+	"name": "Pristine Auctions",
+	"url": "https://pristineauction.com/default/index/search/?search_key=",
+	"element": None,
+	"searchbox": None,
+	"altMethod": "getQuery",
+	"results_element": "class",
+	"results": "productTable",
+})
+
+sites.append({
+	"name": "PWCC Auctions",
+	"url": "http://www.pwccauctions.com/live-auction.php?q=",
+	"element": None,
+	"searchbox": None,
+	"altMethod": "getQuery"
+
+sites.append({
+	"name": "RMY Auctions",
+	"url": "http://www.rmyauctions.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "query",
+	"results": "galleryList",
+})
+
+sites.append({
+	"name": "RR Auctions",
+	"url": "http://www.rrauction.com/browse.cfm?SearchCrit=",
+	"element": None,
+	"searchbox": None,
+	"altMethod": "getQuery",
+})
+
+sites.append({
+	"name": "SCP Auctions",
+	"url": "http://catalog.scpauctions.com/Category/Featured_Items-406.html"
+	"element": "id",
+	"searchbox": "searchTextBox",
+	"altMethod": "searchBy",
+	"altId": "ctl00$ContentPlaceHolder$searchByDropDown",
+	"altKeys": "tt",
+})
+
+sites.append({
+	"name": "Sirius Sports Auctions",
+	"url": "http://siriussportsauctions.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+	"results": "galleryList",
+})
+
+sites.append({
+	"name": "Small Traditions",
+	"url": "http://www.smalltraditions.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+	"results": "galleryList",
+})
+
+sites.append({
+	"name": "Sports World Auctions",
+	"url": "http://www.sportsworldauctions.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+	"results": "galleryList",
+})
+
+sites.append({
+	"name": "Steiner Sports",
+	"url": "http://auction.steinersports.com/catalog.aspx?searchby=3&searchvalue=",
+	"element": None,
+	"searchbox": None,
+	"altmethod": "getQuery",
+	"results": "galleryList",
+})
+
+sites.append({
+	"name": "Sterling Sports Auctions",
+	"url": "http://www.sterlingsportsauctions.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+	"results": "galleryList",
+})
+
+sites.append({
+	"name": "Treasure Tidbits",
+	"url": "https://www.treasuretidbitssportsauction.com/catalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+	"results": "galleryList",
+})
+
+sites.append({
+	"name": "Wheatland Auction Services"<
+	"url": "https://wheatlandauctionservices.hibid.com/auctions/Current?tab=2&group=0&q="
+	"element": None,
+	"searchbox": None,
+	"altMethod": "getQuery",
+	"results": "divCatalogList",
+})
+
+sites.append({
+	"name": "Worthridge Auctions",
+	"url": "http://www.worthridge.com/itemcatalog.aspx",
+	"element": "id",
+	"searchbox": "searchTextBox",
+	"results": "imageTable",
+})
